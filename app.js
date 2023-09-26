408437928f0403f498ff23c60f356954aae2f68f
AsimEqubal <asimequbal.web@gmail.com>
2023-09-26 10:29:32 +0530

    f

--- app.js
+++ app.js
@@ -8,11 +8,29 @@ text +=   value + "<br>"
 text +=   array + "<br>" 
 }*/
 
-const numbers1 = [45, 4, 9, 16, 25];
+/*const numbers1 = [45, 4, 9, 16, 25];
 const numbers2 = numbers1.map(myFunction);
 document.getElementById('semo').innerHTML = numbers2;
 function myFunction(index , array , value){
-return value*3;
-}
+return index*3;
+}*/
+//array filter filter()
+// const numbers1 = [45, 4, 9, 16, 25];
+// const over19 = numbers1.filter(myfun);
+// document.getElementById('semo').innerHTML = over19;
+// function myfun(value, index, array){
+//     return value > 15;
+// }
+
+ const numbers1 = [45, 4, 9,63,45, 4, 9, 16, 2.88,9988,65,63];
+ let sum = numbers1.reduce(myfun);
+ document.getElementById('semo').innerHTML = sum;
+ function myfun(total, value, index, array){
+    return total + value;
+
+ }
+
+
+
 
 
--- document.md
+++ document.md
@@ -1,3 +1,4 @@
 Array iteration methods operate on every array item.
 JavaScript Array forEach()
 The forEach() method calls a function (a callback function) once for each array element.
+//array filter filter() =>
