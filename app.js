e17c510b6cf48aec356923177bd100556518141d
AsimEqubal <asimequbal.web@gmail.com>
2023-09-26 10:27:43 +0530

    new

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const Numbers = [45,69,99,10,12,81,70,75,100,1597];
+/*const Numbers = [45,69,99,10,12,81,70,75,100,1597];
 let text = "";
 Numbers.forEach(nyFunction);
 document.getElementById('semo').innerHTML= text;
@@ -6,4 +6,13 @@ function nyFunction(value,index,array){
 text +=   index + "<br>" 
 text +=   value + "<br>" 
 text +=   array + "<br>" 
+}*/
+
+const numbers1 = [45, 4, 9, 16, 25];
+const numbers2 = numbers1.map(myFunction);
+document.getElementById('semo').innerHTML = numbers2;
+function myFunction(index , array , value){
+return value*3;
 }
+
+
